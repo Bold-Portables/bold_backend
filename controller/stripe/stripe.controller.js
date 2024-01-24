c21bc5e69842239da8616c75d10b292f3e484754
supersakana <zacwilliamson@icloud.com>
2024-01-24 17:20:32 -0600

    removed email that sends when user checks out to pay new subscription

--- controller/stripe/stripe.controller.js
+++ controller/stripe/stripe.controller.js
@@ -24,7 +24,7 @@ exports.createCustomer = async (req, res) => {
         if (!!stripe_customer_id) {
             return apiResponse.successResponseWithData(
                 res,
-                "Stipe costomer already exist",
+                "Stripe customer already exist",
                 { customer: stripe_customer_id }
             );
         }
@@ -35,7 +35,7 @@ exports.createCustomer = async (req, res) => {
         await User.updateOne({ email }, { $set: { stripe_customer_id: id } });
         return apiResponse.successResponseWithData(
             res,
-            "Stipe costomer created successfully"
+            "Stripe customer created successfully"
         );
     } catch (error) {
         return apiResponse.ErrorResponse(res, error.message);
@@ -114,15 +114,15 @@ exports.createCheckoutSession = async (req, res) => {
 
         const { id, url } = session;
 
-        const mailOptions = {
-            from: process.env.MAIL_FROM,
-            to: email,
-            subject: 'Action Required: Payment Confirmation for Service Request',
-            text: `Hi ${user.name},\n\nThank you for your service request. We are pleased to inform you that we have received your request and are in the process of taking action. To proceed with the payment, please click on the following link to make a secure payment via Stripe:\n\n${url}\n\nAlternatively, you can copy and paste the following link in your browser:\n\n${url}\n\nIf you have any questions or need further assistance, please feel free to contact our customer support team.\n\nThank you`,
-            html: `<p>Hi ${user.name},</p><p>Thank you for your service request. We are pleased to inform you that we have received your request and are in the process of taking action.</p><p>To proceed with the payment, please click on the following link to make a secure payment via Stripe:</p><p><a href="${url}">Make Payment</a></p><p>Alternatively, you can copy and paste the following link in your browser:</p><p>${url}</p><p>If you have any questions or need further assistance, please feel free to contact our customer support team.</p><p>Thank you</p>`,
-        };
+        // const mailOptions = {
+        //     from: process.env.MAIL_FROM,
+        //     to: email,
+        //     subject: 'Action Required: Payment Confirmation for Service Request',
+        //     text: `Hi ${user.name},\n\nThank you for your service request. We are pleased to inform you that we have received your request and are in the process of taking action. To proceed with the payment, please click on the following link to make a secure payment via Stripe:\n\n${url}\n\nAlternatively, you can copy and paste the following link in your browser:\n\n${url}\n\nIf you have any questions or need further assistance, please feel free to contact our customer support team.\n\nThank you`,
+        //     html: `<p>Hi ${user.name},</p><p>Thank you for your service request. We are pleased to inform you that we have received your request and are in the process of taking action.</p><p>To proceed with the payment, please click on the following link to make a secure payment via Stripe:</p><p><a href="${url}">Make Payment</a></p><p>Alternatively, you can copy and paste the following link in your browser:</p><p>${url}</p><p>If you have any questions or need further assistance, please feel free to contact our customer support team.</p><p>Thank you</p>`,
+        // };
         
-        mailer.sendMail(mailOptions);
+        // mailer.sendMail(mailOptions);
 
         return apiResponse.successResponseWithData(
             res,
